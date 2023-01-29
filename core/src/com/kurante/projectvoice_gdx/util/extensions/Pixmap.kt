54d208cbaf152cd3f1638168b0c003582b36e5cb
Kurante <kurante2801@gmail.com>
2023-01-29 16:48:24 +0100

    Proper display of level cards

--- core/src/com/kurante/projectvoice_gdx/util/extensions/Pixmap.kt
+++ core/src/com/kurante/projectvoice_gdx/util/extensions/Pixmap.kt
@@ -1,33 +1,35 @@
 package com.kurante.projectvoice_gdx.util.extensions
 
 import com.badlogic.gdx.graphics.Pixmap
+import com.badlogic.gdx.math.MathUtils.ceil
+import com.badlogic.gdx.math.MathUtils.floor
 
+// Legacy background images have an aspect ratio of 1:1
+// yet on legacy they're displayed at an aspect ratio of 16:9 using Unity's AspectRatioFitter (Envelope Parent)
+// This function tries to imitate that
+
+/**
+ * Imitates Unity's Aspect Ratio Fitter when set to Envelope Parent.
+ * Does NOT dispose original Pixmap.
+ *
+ * @param width Resulting width
+ * @param height Resulting height
+ * @param aspectRatio The aspect ratio the original pixmap will be resized to
+*/
 fun Pixmap.crop(
     width: Int,
     height: Int,
     aspectRatio: Float,
-    disposeOld: Boolean = true
 ): Pixmap {
-    // TODO: Tomorrow
-    /*val pixmap = Pixmap(width, height, this.format)
+    val h = height * aspectRatio
+    val pixmap = Pixmap(width, height, this.format)
 
-    if(width / height <= aspectRatio) {
-        val h = ceil(width / aspectRatio)
+    if(width / height < width / h) {
         pixmap.drawPixmap(
             this, 0, 0, this.width, this.height,
-            0, floor(h * 0.5f - height * 0.5f), width, h
+            0, floor((height - h) * 0.5f), width, ceil(h)
         )
-    } else {
-        val w = ceil(width * aspectRatio)
-        pixmap.drawPixmap(
-            this, 0, 0, this.width, this.height,
-            floor(w * 0.5f - width * 0.5f), 0, w, height
-        )
-    }
+    } else TODO()
 
-    if(disposeOld)
-        this.dispose()
-
-    return pixmap*/
-    return this
+    return pixmap
 }
